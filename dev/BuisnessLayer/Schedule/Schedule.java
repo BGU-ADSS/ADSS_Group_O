585e9921821bbdbf71845c1af3ed435677078f94
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2024-05-31 15:47:00 +0300

    working in Schedule

--- dev/BuisnessLayer/Schedule/Schedule.java
+++ dev/BuisnessLayer/Schedule/Schedule.java
@@ -3,6 +3,7 @@ package BuisnessLayer.Schedule;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Dictionary;
+import java.util.Iterator;
 
 import DTOs.ShiftTime;
 
@@ -11,8 +12,12 @@ public class Schedule {
     private Dictionary<LocalDate, Shift[]> dayShifts;
     private Date deadline;
     public Dictionary<LocalDate, Dictionary<ShiftTime, Boolean>> getAvaliableDaysForEmployee(String empId) {
-        for (LocalDate day : dayShifts.keys()) {
-            
+        Iterator<LocalDate> datesInSchedule = dayShifts.keys().asIterator();
+        for (LocalDate day :datesInSchedule. ) {
+            Shift[] shift = dayShifts.get(day);
+            if(dayShifts.get(day).isEmployeeAvaliable(empId)){
+                
+            }
         }
     }
 
