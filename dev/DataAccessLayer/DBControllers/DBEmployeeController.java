cb3f99bee6b554f116b4365f4ec1d8976052cf4d
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2024-06-30 18:57:12 +0300

    adding where statment

--- dev/DataAccessLayer/DBControllers/DBEmployeeController.java
+++ dev/DataAccessLayer/DBControllers/DBEmployeeController.java
@@ -1,5 +1,14 @@
 package DataAccessLayer.DBControllers;
 
+import DataAccessLayer.DBs.EmployeeDB;
+import DataAccessLayer.DBs.RoleForEmployeeDB;
+
 public class DBEmployeeController {
+    private EmployeeDB employeeDBC = new EmployeeDB();
+    private RoleForEmployeeDB rolesDBC = new RoleForEmployeeDB();
+
+
+
+
     
 }
